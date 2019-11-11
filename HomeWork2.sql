f4bacc56ce128cf0e171a49d5f0cef6957b170d8
Kurilo Dmitriy <fobosdem@gmail.com>
2019-11-11 19:37:35 +0200

    edditing 2nd homework

--- HomeWork2.sql
+++ HomeWork2.sql
@@ -2,27 +2,27 @@ use HomeWork2Sql;
 
 select Category from dbo.Sheet1$ group by Category;
 --создание таблицы категорий
-CREATE TABLE [dbo].[Category](
-	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
-	[Catagory] [nvarchar] (100) NOT NULL
-);
-ALTER TABLE [dbo].[Category] ADD UNIQUE ([Catagory]);
+	CREATE TABLE [dbo].[Category](
+		[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+		[Catagory] [nvarchar] (100) NOT NULL
+	);
+	ALTER TABLE [dbo].[Category] ADD UNIQUE ([Catagory]);
 
 --наполнение категорий
 insert into dbo.Category
 select Category from dbo.Sheet1$ group by Category;
 
 --создание таблицы сабкатегорий
-CREATE TABLE [dbo].[Sub-Category](
-	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
-	[Sub-Catagory] [nvarchar] (100) NOT NULL,
-	[Category Id] [int] NOT NULL
-);
-ALTER TABLE [dbo].[Sub-Category] ADD UNIQUE ([Sub-Catagory]);
-ALTER TABLE [dbo].[Sub-Category] WITH CHECK ADD FOREIGN KEY([Category Id]) REFERENCES [dbo].[Category] ([Id]);
+	CREATE TABLE [dbo].[SubCategory](
+		[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+		[SubCatagory] [nvarchar] (100) NOT NULL,
+		[Category Id] [int] NOT NULL
+	);
+	ALTER TABLE [dbo].[SubCategory] ADD UNIQUE ([SubCatagory]);
+	ALTER TABLE [dbo].[SubCategory] WITH CHECK ADD FOREIGN KEY([Category Id]) REFERENCES [dbo].[Category] ([Id]);
 
 --наполнение таблицы сабкатегорий
-insert into dbo.[Sub-Category]
+insert into dbo.[SubCategory]
 select db.[Sub-Category], cat.Id  from dbo.Sheet1$ db
 left join dbo.Category cat ON cat.Catagory = db.Category
 group by db.[Sub-Category], cat.Id;
@@ -30,20 +30,20 @@ group by db.[Sub-Category], cat.Id;
 --Создание таблицы продуктов
 CREATE TABLE [dbo].[Product](
 	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
-	[Producy Id] [nvarchar] (255) NOT NULL,
-	[Product Name] [nvarchar] (255) NOT NULL,
-	[Product Price] [money] not null,
-	[Product Profit] [float] not null,
-	[Sub-Catagory Id] [int] NOT NULL
+	[ProducyId] [nvarchar] (255) NOT NULL,
+	[ProductName] [nvarchar] (255) NOT NULL,
+	[ProductPrice] [money] not null,
+	[ProductProfit] [float] not null,
+	[SubCatagoryId] [int] NOT NULL
 );
-ALTER TABLE [dbo].[Product] ADD UNIQUE ([Producy Id]);
-ALTER TABLE [dbo].[Product] WITH CHECK ADD FOREIGN KEY([Sub-Catagory Id]) REFERENCES dbo.[Sub-Category] ([Id]);
+ALTER TABLE [dbo].[Product] ADD UNIQUE ([ProducyId]);
+ALTER TABLE [dbo].[Product] WITH CHECK ADD FOREIGN KEY([SubCatagoryId]) REFERENCES dbo.[SubCategory] ([Id]);
 
 --наполнение таблицы продуктов
 insert into dbo.Product
 select db.[Product ID], db.[Product Name], ROUND((db.Sales/db.Quantity)/(1-db.Discount), 2), ROUND((db.Profit/(db.Quantity*((db.Sales/db.Quantity)/(1-db.Discount)))+db.Discount),2), subcat.Id
 from dbo.Sheet1$ db
-left join dbo.[Sub-Category] subcat ON subcat.[Sub-Catagory] = db.[Sub-Category]
+left join dbo.[SubCategory] subcat ON subcat.[SubCatagory] = db.[Sub-Category]
 group by db.[Product ID], db.[Product Name], ROUND((db.Sales/db.Quantity)/(1-db.Discount), 2), ROUND((db.Profit/(db.Quantity*((db.Sales/db.Quantity)/(1-db.Discount)))+db.Discount),2), subcat.Id;
 
 
@@ -152,14 +152,14 @@ left join dbo.Segment seg on seg.Segment = db.Segment
 group by db.[Customer ID], db.[Customer Name], seg.Id;
 
 --создание Ship mode
-CREATE TABLE [dbo].[Ship Mode](
+CREATE TABLE [dbo].[ShipMode](
 	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
 	[ShipModeName] [nvarchar] (255) NOT NULL
 );
-ALTER TABLE [dbo].[Ship Mode] ADD UNIQUE ([ShipModeName]);
+ALTER TABLE [dbo].[ShipMode] ADD UNIQUE ([ShipModeName]);
 
 --наполнение Country
-insert into dbo.[Ship Mode]
+insert into dbo.[ShipMode]
 select db.[Ship Mode]
 from dbo.Sheet1$ db
 group by db.[Ship Mode];
@@ -169,31 +169,49 @@ group by db.[Ship Mode];
 CREATE TABLE [dbo].[Order](
 	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
 	[OrderId] [varchar] (255) NOT NULL,
-	[Order Date] [datetime] NOT NULL,
-	[Ship Date] [datetime] NOT NULL,
+	[OrderDate] [datetime] NOT NULL,
+	[ShipDate] [datetime] NOT NULL,
 	[ShipModeId] [int] NOT NULL,
 	[PostalCodeId] [int] NOT NULL,
-	[CustomerId] [int] NOT NULL,
+	[CustomerId] [int] NOT NULL
+);
+ALTER TABLE [dbo].[Order] WITH CHECK ADD FOREIGN KEY([ShipModeId]) REFERENCES dbo.[ShipMode] ([Id]);
+ALTER TABLE [dbo].[Order] WITH CHECK ADD FOREIGN KEY([PostalCodeId]) REFERENCES dbo.[PostalCode] ([Id]);
+ALTER TABLE [dbo].[Order] WITH CHECK ADD FOREIGN KEY([CustomerId]) REFERENCES dbo.Customer ([Id]);
+
+---наполнение таблицы Order
+insert into dbo.[Order]
+select db.[Order ID], db.[Order Date], db.[Ship Date], sm.Id, pc.Id, cust.Id
+from dbo.Sheet1$ db
+left join dbo.[ShipMode] sm on sm.ShipModeName = db.[Ship Mode]
+left join dbo.PostalCode pc on pc.PostalCode = db.[Postal Code]
+left join dbo.Customer cust on cust.CustomerId = db.[Customer ID]
+group by db.[Order ID], db.[Order Date], db.[Ship Date], sm.Id, pc.Id, cust.Id;
+
+
+
+
+
+
+CREATE TABLE [dbo].[OrderDetails](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[OrderId] [int] NOT NULL,
 	[ProductId] [int] NOT NULL,
 	[Quantity] [int] NOT NULL,
 	[Discount] [float] NOT NULL,
 	[Profit] [money] NOT NULL
 );
-ALTER TABLE [dbo].[Order] WITH CHECK ADD FOREIGN KEY([ShipModeId]) REFERENCES dbo.[Ship Mode] ([Id]);
-ALTER TABLE [dbo].[Order] WITH CHECK ADD FOREIGN KEY([PostalCodeId]) REFERENCES dbo.[PostalCode] ([Id]);
-ALTER TABLE [dbo].[Order] WITH CHECK ADD FOREIGN KEY([CustomerId]) REFERENCES dbo.Customer ([Id]);
-ALTER TABLE [dbo].[Order] WITH CHECK ADD FOREIGN KEY([ProductId]) REFERENCES dbo.Product ([Id]);
+ALTER TABLE [dbo].[OrderDetails] ADD CONSTRAINT uniqOrderDet UNIQUE ([OrderId], [ProductId], [Quantity], [Discount]);
+ALTER TABLE [dbo].[OrderDetails] WITH CHECK ADD FOREIGN KEY([ProductId]) REFERENCES dbo.Product ([Id]);
+ALTER TABLE [dbo].[OrderDetails] WITH CHECK ADD FOREIGN KEY([OrderId]) REFERENCES dbo.[Order] ([Id]);
 
----наполнение таблицы Order
-insert into dbo.[Order]
-select db.[Order ID], db.[Order Date], db.[Ship Date], sm.Id, pc.Id, cust.Id, prod.Id, db.Quantity, db.Discount, db.Profit
+insert into dbo.[OrderDetails]
+select ord.Id, prod.Id, db.Quantity, db.Discount, db.Profit
 from dbo.Sheet1$ db
-left join dbo.[Ship Mode] sm on sm.ShipModeName = db.[Ship Mode]
-left join dbo.PostalCode pc on pc.PostalCode = db.[Postal Code]
-left join dbo.Customer cust on cust.CustomerId = db.[Customer ID]
-left join dbo.Product prod on prod.[Producy Id] = db.[Product ID]
-group by db.[Order ID], db.[Order Date], db.[Ship Date], sm.Id, pc.Id, cust.Id, prod.Id, db.Quantity, db.Discount, db.Profit;
+left join dbo.[Order] ord on ord.OrderId = db.[Order ID]
+left join dbo.Product prod on prod.ProducyId = db.[Product ID]
+group by ord.Id, prod.Id, db.Quantity, db.Discount, db.Profit;
 
 --общий профит
 select SUM(db.Profit)
-from dbo.[Order] db;
+from dbo.[OrderDetails] db;
